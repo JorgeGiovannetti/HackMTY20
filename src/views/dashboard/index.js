d8821b0f51aeb65d8173a747ab0a7d5995ee170f
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 16:29:22 -0500

    Dashboard and login views

---
+++ src/views/dashboard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AppLayout from "../../common/components/AppLayout";
+
+const Dashboard = () => (
+	<AppLayout>
+		<div>Dashboard</div>
+	</AppLayout>
+);
+
+export default Dashboard;
