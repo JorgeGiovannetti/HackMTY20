3148ffcebc0e15fc70a1bfd398e83d6415d22a37
Jorge Abdo <jorgeabdo@jorges-mbp.lan>
2020-08-29 16:56:05 -0500

    conflicts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
 import firebase from "firebase";
 import Login from "./views/login/login";
-<<<<<<< HEAD
 import Cam from "./templates";
 import SideBar from "./common/components/SideNav";
-=======
 import Routes from "./common/utils/routes";
->>>>>>> 24e9bdbff80e021d2d03fd808b3394b3aa9ed062
 
 const App = () => {
-	const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState(false);
 
-<<<<<<< HEAD
   firebase.auth().onAuthStateChanged((user) => {
     if (user != null) {
       setIsLogged(true);
@@ -19,17 +15,7 @@ const App = () => {
       setIsLogged(false);
     }
   });
-  return <div>{isLogged ? <SideBar /> : <Login />}</div>;
-=======
-	firebase.auth().onAuthStateChanged((user) => {
-		if (user != null) {
-			setIsLogged(true);
-		} else {
-			setIsLogged(false);
-		}
-	});
-	return <div>{isLogged ? <Routes /> : <Login />}</div>;
->>>>>>> 24e9bdbff80e021d2d03fd808b3394b3aa9ed062
+  return <div>{isLogged ? <Routes /> : <Login />}</div>;
 };
 
 export default App;
