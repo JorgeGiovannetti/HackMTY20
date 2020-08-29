9e140fb70e846e863b67e10c4dbf35b9fc02689e
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 12:49:45 -0500

    React init + antd

---
+++ posty/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "antd";
+
+const App = () => {
+	return (
+		<div>
+			<header>POSTy :D</header>
+			<Button>Hi</Button>
+		</div>
+	);
+};
+
+export default App;
