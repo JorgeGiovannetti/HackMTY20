dcf456117d4cd5f1ea065086b58356079d7c0615
Jorge Abdo <jorgeabdo@jorges-mbp.lan>
2020-08-29 13:28:38 -0500

    firebase swr

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import firebase from "firebase"
 import { Button } from "antd";
 
 const App = () => {
+	const [isLogged, setIsLogged] = useState(false);
+
+	firebase.auth().onAuthStateChanged((user) => {
+		if (user != null) {
+		  setIsLogged(true);
+		} else {
+		  setIsLogged(false);
+		}
+	  });
+
 	return (
-		<div>
-			<header>POSTy :D</header>
-			<Button>Hi</Button>
-		</div>
-	);
+        <div>{isLogged ? <Button>Login</Button> : <Button>Dashboard</Button>}</div>
+      );
 };
 
 export default App;
