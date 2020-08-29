1938813cd5a8ebe00899e8c0825395be0a79081d
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 16:28:45 -0500

    SideNav with logout

--- src/common/components/SideNav/index.js
+++ src/common/components/SideNav/index.js
@@ -1,47 +1,49 @@
-import React, { Component } from "react";
-import { Menu, Layout } from "antd";
+import React from "react";
+import { Menu, Layout, Button } from "antd";
 import {
 	VideoCameraOutlined,
 	DatabaseOutlined,
 	FormOutlined,
 } from "@ant-design/icons";
 import { Link, withRouter } from "react-router-dom";
+import firebase from "firebase";
 
 const { Sider } = Layout;
 
-class SideNav extends Component {
-	render() {
-		const {
-			location: { pathname },
-		} = this.props;
+const logout = () => {
+	firebase.auth().signOut();
+};
 
-		return (
-			<Sider>
-				<div className="logo" />
-				<Menu
-					theme="light"
-					mode="inline"
-					selectedKeys={["/" + pathname.split("/")[1]]}
-				>
-					<Menu.Item key="/">
-						<Link to="/"></Link>
-						<VideoCameraOutlined />
-						<span className="nav-text">Video Feed</span>
-					</Menu.Item>
-					<Menu.Item key="/Analytics">
-						<Link to="/"></Link>
-						<DatabaseOutlined />
-						<span className="nav-text">Analytics</span>
-					</Menu.Item>
-					<Menu.Item key="/reports">
-						<Link to="/"></Link>
-						<FormOutlined />
-						<span className="nav-text">Reports</span>
-					</Menu.Item>
-				</Menu>
-			</Sider>
-		);
-	}
-}
+const SideNav = ({ location }) => {
+	const { pathname } = location;
+
+	return (
+		<Sider>
+			<div className="logo" />
+			<Menu
+				theme="light"
+				mode="inline"
+				selectedKeys={["/" + pathname.split("/")[1]]}
+			>
+				<Menu.Item key="/">
+					<Link to="/"></Link>
+					<VideoCameraOutlined />
+					<span className="nav-text">Video Feed</span>
+				</Menu.Item>
+				<Menu.Item key="/Analytics">
+					<Link to="/"></Link>
+					<DatabaseOutlined />
+					<span className="nav-text">Analytics</span>
+				</Menu.Item>
+				<Menu.Item key="/reports">
+					<Link to="/"></Link>
+					<FormOutlined />
+					<span className="nav-text">Reports</span>
+				</Menu.Item>
+				<Button onClick={logout}>Log Out</Button>
+			</Menu>
+		</Sider>
+	);
+};
 
 export default withRouter(SideNav);
