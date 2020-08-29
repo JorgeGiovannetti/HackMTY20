e4872cc34073a3fd9e79a495637831581b90d2c6
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 18:22:17 -0500

    Refactor file tree

--- src/index.js
+++ src/index.js
@@ -3,27 +3,27 @@ import ReactDOM from "react-dom";
 import App from "./App.js";
 import { ThemeProvider } from "styled-components";
 import "antd/dist/antd.css";
-import theme from "./theme";
+import theme from "./common/theme";
 import { Fuego, FuegoProvider } from "@nandorojo/swr-firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtvcqizYRMcQOfH6jfbBps4WuQ0LzqxcY",
-  authDomain: "posty-ecd9b.firebaseapp.com",
-  databaseURL: "https://posty-ecd9b.firebaseio.com",
-  projectId: "posty-ecd9b",
-  storageBucket: "posty-ecd9b.appspot.com",
-  messagingSenderId: "816714854768",
-  appId: "1:816714854768:web:22b28c1250a2ad471b1fb5",
+	apiKey: "AIzaSyBtvcqizYRMcQOfH6jfbBps4WuQ0LzqxcY",
+	authDomain: "posty-ecd9b.firebaseapp.com",
+	databaseURL: "https://posty-ecd9b.firebaseio.com",
+	projectId: "posty-ecd9b",
+	storageBucket: "posty-ecd9b.appspot.com",
+	messagingSenderId: "816714854768",
+	appId: "1:816714854768:web:22b28c1250a2ad471b1fb5",
 };
 const fuego = new Fuego(firebaseConfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <FuegoProvider fuego={fuego}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </FuegoProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<FuegoProvider fuego={fuego}>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</FuegoProvider>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
--- src/views/dashboard/feed/index.js
+++ src/views/dashboard/feed/index.js
@@ -7,74 +7,74 @@ import Avatar from "../common/avatar";
 import Tooltip from "../common//tooltip";
 import Message from "./components/message";
 import {
-  Container,
-  WhiteBox,
-  MessagesContainer,
-  Scroll,
-  CloseButton,
-  // ActionButton,
+	Container,
+	WhiteBox,
+	MessagesContainer,
+	Scroll,
+	CloseButton,
+	// ActionButton,
 } from "./elements";
 
 class ChatContent extends Component {
-  state = {
-    message: "",
-  };
+	state = {
+		message: "",
+	};
 
-  setRef = (ref) => {
-    if (ref) {
-      // eslint-disable-next-line no-param-reassign
-      ref.scrollTop = ref.scrollHeight;
-    }
-  };
+	setRef = (ref) => {
+		if (ref) {
+			// eslint-disable-next-line no-param-reassign
+			ref.scrollTop = ref.scrollHeight;
+		}
+	};
 
-  handleMessageChange = ({ target: { value } }) =>
-    this.setState({ message: value });
+	handleMessageChange = ({ target: { value } }) =>
+		this.setState({ message: value });
 
-  render() {
-    const { user, closeChat, profile, messages } = this.props;
-    if (user !== null) {
-      return (
-        <Container>
-          <WhiteBox height={70} top="0">
-            <Box p={10} display="flex" alignItems="center">
-              <Avatar
-                mr={10}
-                size={50}
-                src={"/static/img/general/avatar.png"}
-              />
-              <Typography mr="auto" variant="leadText">
-                Feed
-              </Typography>
-              <Tooltip tag="Close chat">
-                <CloseButton onClick={closeChat} variant="link" color="danger">
-                  <MdClear />
-                </CloseButton>
-              </Tooltip>
-            </Box>
-          </WhiteBox>
-          <Scroll ref={this.setRef}>
-            <MessagesContainer>
-              {messages &&
-                // eslint-disable-next-line no-shadow
-                messages.map(({ id, message, timestamp, sender }) => (
-                  <Message
-                    otherProfileImg={user.profileImg}
-                    yourImage={profile.profileImg}
-                    key={id}
-                    message={message}
-                    sentAt={timestamp}
-                    seenAt={message}
-                    isYours={profile.userID === sender}
-                  />
-                ))}
-            </MessagesContainer>
-          </Scroll>
-          <WhiteBox height={60} bottom="0"></WhiteBox>
-        </Container>
-      );
-    }
-    return null;
-  }
+	render() {
+		const { user, closeChat, profile, messages } = this.props;
+		if (user !== null) {
+			return (
+				<Container>
+					<WhiteBox height={70} top="0">
+						<Box p={10} display="flex" alignItems="center">
+							<Avatar
+								mr={10}
+								size={50}
+								src={"/static/img/general/avatar.png"}
+							/>
+							<Typography mr="auto" variant="leadText">
+								Feed
+							</Typography>
+							<Tooltip tag="Close chat">
+								<CloseButton onClick={closeChat} variant="link" color="danger">
+									<MdClear />
+								</CloseButton>
+							</Tooltip>
+						</Box>
+					</WhiteBox>
+					<Scroll ref={this.setRef}>
+						<MessagesContainer>
+							{messages &&
+								// eslint-disable-next-line no-shadow
+								messages.map(({ id, message, timestamp, sender }) => (
+									<Message
+										otherProfileImg={user.profileImg}
+										yourImage={profile.profileImg}
+										key={id}
+										message={message}
+										sentAt={timestamp}
+										seenAt={message}
+										isYours={profile.userID === sender}
+									/>
+								))}
+						</MessagesContainer>
+					</Scroll>
+					<WhiteBox height={60} bottom="0"></WhiteBox>
+				</Container>
+			);
+		}
+		return null;
+	}
 }
 
 export default ChatContent;
