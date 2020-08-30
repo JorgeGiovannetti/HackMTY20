95d22076033284468fb4545418cd7b275f822c88
Jorge Abdo <jorgeabdo@Jorges-MacBook-Pro.local>
2020-08-29 23:32:47 -0500

    push

--- src/camera.py
+++ src/camera.py
@@ -4,10 +4,13 @@ import numpy as np
 import imutils
 import cv2
 import math
-
+import random
+import requests
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+MAX_DIST = 75
+stores = ["A", "B", "C", "D"]
 
 class VideoCamera(object):
 	def __init__(self):
@@ -17,27 +20,36 @@ class VideoCamera(object):
 		self.video.release()
 	
 	def detect_people(self, frame):
-		#height, width, channel = frame.shape
-		#frame = cv2.resize(frame, (320, 240))
-		
-		
-		scale_h = 1#height/320
-		scale_w = 1#width/240
-		
-		
 		gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-		#ret,frame = cv2.threshold(frame,80,255,cv2.THRESH_BINARY)
 		
 		(rects, weights) = hog.detectMultiScale(frame, winStride=(8, 8), scale=1.05)
-		rects = np.array([[x*scale_w, y*scale_h, (x + w)*scale_w, (y + h)*scale_h] for (x, y, w, h) in rects])
+		rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
 		pick = non_max_suppression(rects, probs=None, overlapThresh=0.5)
 	
 		return pick
 
+	def get_violating_distances(self, people_squares):
+		violations = set()
+		if len(people_squares) > 2:
+			for (x_a_first, y_a_first, x_b_first, y_b_first) in people_squares:
+				for (x_a_snd, y_a_snd, x_b_snd, y_b_snd) in people_squares:
+					if (x_a_first, y_a_first, x_b_first, y_b_first) != (x_a_snd, y_a_snd, x_b_snd, y_b_snd):
+						center_x_first, center_y_first = ((x_a_first + x_b_first)/2, (y_a_first + y_b_first)/2)
+						center_x_snd, center_y_snd = ((x_a_snd + x_b_snd)/2, (y_a_snd + y_b_snd)/2)
+						dist = math.sqrt(abs(center_x_snd - center_x_first)*2 + abs(center_y_snd - center_y_snd)*2)
+						if (dist < MAX_DIST):
+							report = {"timestamp": time.time(), "error": "Social distance violated", "store": stores[random.randint(0, 3)]}
+							requests.post(report_endpoint, json=report)
+							violations.add((x_a_first, y_a_first, x_b_first, y_b_first))
+							violations.add((x_a_first, y_a_first, x_b_first, y_b_first))
+		return violations
+
 	def get_frame(self):
 		ret, frame = self.video.read()
 		people_squares = self.detect_people(frame)
+		violations = self.get_violating_distances(people_squares)
 		for (xA, yA, xB, yB) in people_squares:
-        		cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        		cv2.rectangle(frame, (xA, yA), (xB, yB),
+				(0, 255, 0) if (xA, yA, xB, yB) not in violations else (0, 0, 255), 2)
 		ret, jpeg = cv2.imencode('.jpg', frame)
 		return jpeg.tobytes()
