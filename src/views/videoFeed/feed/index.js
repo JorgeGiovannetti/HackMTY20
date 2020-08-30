65349f140a0b233f474df58ace530397928df05e
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-30 02:02:50 -0500

    Cleanup

--- src/views/videoFeed/feed/index.js
+++ src/views/videoFeed/feed/index.js
@@ -3,104 +3,99 @@ import firebase from "firebase";
 import Typography from "../common/typography";
 import Box from "../common//box";
 import { MdClear } from "react-icons/md";
-import Avatar from "../common/avatar";
 import Tooltip from "../common/tooltip";
 import Message from "./components/message";
 import {
-  Container,
-  WhiteBox,
-  MessagesContainer,
-  Scroll,
-  CloseButton,
-  // ActionButton,
+	Container,
+	WhiteBox,
+	MessagesContainer,
+	Scroll,
+	CloseButton,
 } from "./elements";
 
 class ChatContent extends Component {
-  state = {
-    messages: [],
-  };
+	state = {
+		messages: [],
+	};
 
-  componentDidMount() {
-    this.getMessages();
-  }
+	componentDidMount() {
+		this.getMessages();
+	}
 
-  setRef = (ref) => {
-    if (ref) {
-      // eslint-disable-next-line no-param-reassign
-      ref.scrollTop = ref.scrollHeight;
-    }
-  };
+	setRef = (ref) => {
+		if (ref) ref.scrollTop = ref.scrollHeight;
+	};
 
-  getMessages = (chat) => {
-    this.setState((prevState) => ({
-      messages: null,
-    }));
-    const messagesRef = firebase.database().ref(`/reports`).limitToLast(100);
+	getMessages = (chat) => {
+		this.setState((prevState) => ({
+			messages: null,
+		}));
+		const messagesRef = firebase.database().ref(`/reports`).limitToLast(100);
 
-    messagesRef.on("value", (snapshot) => {
-      if (snapshot.empty) {
-        this.setState((prevState) => ({
-          messages: null,
-        }));
-      }
-      let messagesObj = snapshot.val();
-      let messages = [];
-      if (messagesObj !== null) {
-        Object.keys(messagesObj).forEach((key) =>
-          messages.push(messagesObj[key])
-        );
-        messages = messages.map((message) => {
-          return {
-            error: message.error,
-            horas: message.horas,
-            timestamp: message.timestamp,
-          };
-        });
-        this.setState((prevState) => ({
-          messages: messages,
-        }));
-      }
-    });
-  };
+		messagesRef.on("value", (snapshot) => {
+			if (snapshot.empty) {
+				this.setState((prevState) => ({
+					messages: null,
+				}));
+			}
+			let messagesObj = snapshot.val();
+			let messages = [];
+			if (messagesObj !== null) {
+				Object.keys(messagesObj).forEach((key) =>
+					messages.push(messagesObj[key])
+				);
+				messages = messages.map((message) => {
+					return {
+						error: message.error,
+						horas: message.horas,
+						timestamp: message.timestamp,
+					};
+				});
+				this.setState((prevState) => ({
+					messages: messages,
+				}));
+			}
+		});
+	};
 
-  handleMessageChange = ({ target: { value } }) =>
-    this.setState({ message: value });
+	handleMessageChange = ({ target: { value } }) =>
+		this.setState({ message: value });
 
-  render() {
-    const { closeChat } = this.props;
-    const { messages } = this.state;
+	render() {
+		const { closeChat } = this.props;
+		const { messages } = this.state;
 
-    return (
-      <Container>
-        <WhiteBox height={30} top="0">
-          <Box p={10} display="flex" alignItems="center">
-            <Typography mr="auto" variant="leadText">
-              Feed
-            </Typography>
-            <Tooltip tag="Close chat">
-              <CloseButton onClick={closeChat} variant="link" color="danger">
-                <MdClear />
-              </CloseButton>
-            </Tooltip>
-          </Box>
-        </WhiteBox>
-        <Scroll ref={this.setRef}>
-          <MessagesContainer>
-            {messages &&
-              // eslint-disable-next-line no-shadow
-              messages.map(({ id, error, timestamp }) => (
-                <Message
-                  key={id}
-                  message={error}
-                  sentAt={timestamp}
-                  isYours={true}
-                />
-              ))}
-          </MessagesContainer>
-        </Scroll>
-      </Container>
-    );
-  }
+		return (
+			<Container>
+				<WhiteBox height={30} top="0">
+					<Box p={10} display="flex" alignItems="center">
+						<Typography mr="auto" variant="leadText">
+							Feed
+						</Typography>
+						<Tooltip tag="Close chat">
+							<CloseButton onClick={closeChat} variant="link" color="danger">
+								<MdClear />
+							</CloseButton>
+						</Tooltip>
+					</Box>
+				</WhiteBox>
+				<Scroll ref={this.setRef}>
+					<MessagesContainer>
+						{messages &&
+							// eslint-disable-next-line no-shadow
+							messages.map(({ id, error, timestamp }) => (
+								<Message
+									key={id}
+									message={error}
+									sentAt={timestamp}
+									isYours={true}
+								/>
+							))}
+					</MessagesContainer>
+				</Scroll>
+			</Container>
+		);
+	}
 }
 
 export default ChatContent;
