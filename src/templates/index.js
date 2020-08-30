530cfe47cd267ee9b6e9ae3014c2e6775d0db9b0
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 19:07:13 -0500

    Attributes no longer needed

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Cam = ({ width, height }) => (
-	<img src="http://localhost:5000/video_feed" alt="Video" />
-);
+const Cam = () => <img src="http://localhost:5000/video_feed" alt="Video" />;
 
 export default Cam;
