44be7dfe8756ecc85ee23ffb3da4d9ef3e30ca5d
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 19:06:41 -0500

    Layout modified

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Cam = ({ width, height }) => (
-  <img
-    src="http://localhost:5000/video_feed"
-    alt="Video"
-    style={{ width: width ? width : "70%", height: height ? height : "100%" }}
-  />
+	<img src="http://localhost:5000/video_feed" alt="Video" />
 );
 
 export default Cam;
