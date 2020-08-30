c8ce0d94e69cf7aafbd60dd4876efba71dabebd6
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-30 10:27:48 -0500

    UI sketch with container + props

--- src/views/sketch/common/SketchBuilder/index.js
+++ src/views/sketch/common/SketchBuilder/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Stage, Layer, Circle, Text } from "react-konva";
+import Container from "../../../../common/components/Container";
 
 function generateShapes() {
 	return [...Array(10)].map((_, i) => ({
@@ -13,7 +14,7 @@ function generateShapes() {
 
 const INITIAL_STATE = generateShapes();
 
-const SketchBuilder = () => {
+const SketchBuilder = ({ diameter, quota, tables }) => {
 	const [circles, setCircles] = React.useState(INITIAL_STATE);
 
 	const handleDragCirclet = (e) => {
@@ -39,32 +40,34 @@ const SketchBuilder = () => {
 	};
 
 	return (
-		<Stage width={window.innerWidth} height={window.innerHeight}>
-			<Layer>
-				{circles.map((circle) => (
-					<Circle
-						key={circle.id}
-						id={circle.id}
-						x={circle.x}
-						y={circle.y}
-						radius={window.innerWidth / 50}
-						fill="#89b717"
-						opacity={0.8}
-						draggable
-						rotation={circle.rotation}
-						shadowColor="black"
-						shadowBlur={10}
-						shadowOpacity={0.6}
-						shadowOffsetX={circle.isDragging ? 10 : 5}
-						shadowOffsetY={circle.isDragging ? 10 : 5}
-						scaleX={circle.isDragging ? 1.2 : 1}
-						scaleY={circle.isDragging ? 1.2 : 1}
-						onDragCirclet={handleDragCirclet}
-						onDragEnd={handleDragEnd}
-					/>
-				))}
-			</Layer>
-		</Stage>
+		<Container style={{ marginRight: "10%" }}>
+			<Stage width={window.innerWidth * 0.75} height={window.innerHeight}>
+				<Layer>
+					{circles.map((circle) => (
+						<Circle
+							key={circle.id}
+							id={circle.id}
+							x={circle.x}
+							y={circle.y}
+							radius={window.innerWidth / 50}
+							fill="#89b717"
+							opacity={0.8}
+							draggable
+							rotation={circle.rotation}
+							shadowColor="black"
+							shadowBlur={10}
+							shadowOpacity={0.6}
+							shadowOffsetX={circle.isDragging ? 10 : 5}
+							shadowOffsetY={circle.isDragging ? 10 : 5}
+							scaleX={circle.isDragging ? 1.2 : 1}
+							scaleY={circle.isDragging ? 1.2 : 1}
+							onDragCirclet={handleDragCirclet}
+							onDragEnd={handleDragEnd}
+						/>
+					))}
+				</Layer>
+			</Stage>
+		</Container>
 	);
 };
 
