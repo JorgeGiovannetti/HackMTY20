625b1c081da537fba4d927dfa7672e7a1dd44c7d
JorgeGiovannetti <giovannetti2000@gmail.com>
2020-08-29 20:33:23 -0500

    VideoFeed fallback handling

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Spin } from "antd";
 
-const Cam = () => <img src="http://localhost:5000/video_feed" alt="Video" />;
+const Cam = () => {
+	const [camError, setCamError] = useState(false);
+	const [loading, setLoading] = useState(true);
+
+	const onCamError = () => setCamError(true);
+	const onLoad = () => setLoading(false);
+
+	return (
+		<div style={{ marginRight: "5%" }}>
+			{(camError || loading) && (
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						width: "500px",
+						height: "500px",
+						justifyContent: "center",
+						alignItems: "center",
+					}}
+				>
+					{camError ? (
+						<img
+							style={{ borderRadius: "15px" }}
+							src={require("../common/img/VideoOffline.png")}
+						/>
+					) : (
+						<Spin size="large" />
+					)}
+				</div>
+			)}
+			<img
+				style={{ display: loading ? "none" : "block", borderRadius: "15px" }}
+				src="http://localhost:5000/video_feed"
+				alt="Video"
+				onError={onCamError}
+				onLoad={onLoad}
+			/>
+		</div>
+	);
+};
 
 export default Cam;
